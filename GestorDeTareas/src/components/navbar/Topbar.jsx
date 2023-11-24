46fe22c7c8120f639cc85063bf451e7963e36141
Lucas Szapowal <powal.012@gmail.com>
2023-11-24 18:00:18 -0300

    navbar

---
+++ GestorDeTareas/src/components/navbar/Topbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.css"
+import { Link } from 'react-router-dom';
+
+export default function TopBar() {
+    return (
+        <div className="navbar">
+           
+                <div className="logo">
+                    <img src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr" alt="Logo" />
+                </div>
+                <h1 className="title">Tus Tareas</h1>
+                <div className="links">
+                    <ul className="list">
+                        <li className="item"><Link to="/">Inicio</Link></li>
+                        <li className="item"><Link to="/creartarea">Crear Tarea</Link></li>
+                        <li className="item"><Link to="/listadetareas">Lista de tareas</Link></li>
+                        <li className="item"><Link to="/contacto"> Contacto</Link></li>
+                    </ul>
+                </div>
+           
+
+        </div>
+    )
+}
