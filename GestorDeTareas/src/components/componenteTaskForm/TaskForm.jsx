0e219de19401bce31e1efe1c858ecd719d510839
Caronila <carolinacardoso92@gmail.com>
2023-11-26 17:01:04 -0300

    TaskForm creado

--- GestorDeTareas/src/components/componenteTaskForm/TaskForm.jsx
+++ GestorDeTareas/src/components/componenteTaskForm/TaskForm.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
 
 const TaskForm = () => {
+
+    const [newTask, setNewTask] = useState ()
+
+    const handleSubmit = (e) => {
+        e.preventDefault (); 
+        localStorage.setItem('task', newTask)
+        setNewTask('')
+
+    }
     return (
         <div>
-            <h1>...Acá va el formulario...</h1>
+            <h1>Agregar tareas</h1>
+            <form onSubmit={handleSubmit}>
+               <label>Nueva tarea</label> <input type='text' value={newTask} onChange={(e) => setNewTask (e.target.value)}></input>
+                <button onClick={() => (newTask)}>Guardar tarea</button>
+            </form>
         </div>
     );
 }
--- GestorDeTareas/src/components/componenteTaskList/TaskList.jsx
+++ GestorDeTareas/src/components/componenteTaskList/TaskList.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import TaskItem from '../componenteTaskItem/TaskItem';
+import TaskForm from '../componenteTaskForm/TaskForm';
 
 const TaskList = () => {
     return (
         <div>
             <h1>...Acá va la Lista de tareas...</h1>
             <TaskItem/>
-
+            <TaskForm/>
         </div>
+       
     );
 }
 
