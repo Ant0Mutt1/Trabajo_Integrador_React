a071e6058d37bac36b3392245dfd280d40a646ff
Caronila <carolinacardoso92@gmail.com>
2023-11-26 17:21:12 -0300

    Dandole funcionalidad a tasklist

--- GestorDeTareas/src/components/componenteTaskList/TaskList.jsx
+++ GestorDeTareas/src/components/componenteTaskList/TaskList.jsx
@@ -1,15 +1,40 @@
+import { useState } from 'react';
 import React from 'react';
 import TaskItem from '../componenteTaskItem/TaskItem';
 import TaskForm from '../componenteTaskForm/TaskForm';
 
 const TaskList = () => {
+
+    const [taskItem, setTaskItem] = useState([
+        { name: 'mi tarea', done: false }
+    ]);
+
     return (
         <div>
-            <h1>...Acá va la Lista de tareas...</h1>
-            <TaskItem/>
-            <TaskForm/>
+            <TaskForm />
+            <h1>Mis tareas</h1>
+            <TaskItem />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Tareas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        taskItem.map(task => (
+                            <tr key={task.name}>
+                                <td> {task.name}</td>
+                            </tr>
+
+                        ))
+                    }
+                </tbody>
+            </table>
+
+
         </div>
-       
+
     );
 }
 
