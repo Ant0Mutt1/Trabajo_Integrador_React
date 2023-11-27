6a76c1ef0106a097ff1a74326de923a8f9a1841f
Lucas Szapowal <powal.012@gmail.com>
2023-11-27 01:47:10 -0300

    Add files via upload

--- GestorDeTareas/src/components/componenteTaskItem/TaskItem.jsx
+++ GestorDeTareas/src/components/componenteTaskItem/TaskItem.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import confetti from "canvas-confetti"
+import "./taskitem.css"
 
 const TaskItem = ({ task, onDelete, onToggleComplete }) => {
   return (
-    <tr key={task.name} style={{ textDecoration: task.done ? 'line-through' : 'none' }}>
-      <td>{task.name}</td>
+    <tr  className="trt" key={task.name} style={{ textDecoration: task.done ? 'line-through' : 'none' }}>
+      <td className="tdt">{task.name}</td>
       <td>
         <button className='btn btn-danger'  onClick={() => onDelete(task.name)}>Borrar</button>
-        <button className='btn btn-success'  onClick={() => onToggleComplete(task.name)}>Completar</button>
+        <button
+  className='btn btn-success'
+  onClick={() => { onToggleComplete(task.name); confetti();}}>Completar</button>
       </td>
     </tr>
   );
