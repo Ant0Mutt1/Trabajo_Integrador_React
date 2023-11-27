23f4ba1b24990c0fcf8569d4c1f4846ae69f97d2
Lucas Szapowal <powal.012@gmail.com>
2023-11-27 01:46:47 -0300

    Add files via upload

--- GestorDeTareas/src/components/componenteTaskList/TaskList.jsx
+++ GestorDeTareas/src/components/componenteTaskList/TaskList.jsx
@@ -1,51 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskItem from '../componenteTaskItem/TaskItem';
 import TaskForm from '../componenteTaskForm/TaskForm';
+import "./tasklist.css"
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
 
-  const createNewTask = (taskName) => {
-    const exists = tasks.find((task) => task.name === taskName);
-    if (!exists) {
-      setTasks([...tasks, { name: taskName, done: false }]);
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
     }
+  }, []);
+
+  const createNewTask = (taskName) => {
+    setTasks((prevTasks) => {
+      const exists = prevTasks.some((task) => task.name === taskName);
+      if (!exists) {
+        const newTasks = [...prevTasks, { name: taskName, done: false }];
+        localStorage.setItem('tasks', JSON.stringify(newTasks));
+        return newTasks;
+      }
+      return prevTasks;
+    });
   };
 
   const deleteTask = (taskName) => {
-    setTasks(tasks.filter((task) => task.name !== taskName));
+    setTasks((prevTasks) => {
+      const newTasks = prevTasks.filter((task) => task.name !== taskName);
+      localStorage.setItem('tasks', JSON.stringify(newTasks));
+      return newTasks;
+    });
   };
 
   const toggleComplete = (taskName) => {
-    setTasks(
-      tasks.map((task) =>
+    setTasks((prevTasks) => {
+      const newTasks = prevTasks.map((task) =>
         task.name === taskName ? { ...task, done: !task.done } : task
-      )
-    );
+      );
+      localStorage.setItem('tasks', JSON.stringify(newTasks));
+      return newTasks;
+    });
   };
 
   return (
-    <div className='container-fluid d-flex  flex-column align-items-center bg-secondary'>
-      <TaskForm onCreateTask={createNewTask} />
-      <h1>Mis tareas</h1>
-      <table className='table w-50 table-dark table-striped-row '>
-        <thead>
-          <tr>
-            <th className='col-2'>Tareas</th>
-            <th className='col-1'>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
-            <TaskItem
-              key={task.name}
-              task={task}
-              onDelete={deleteTask}
-              onToggleComplete={toggleComplete}
-            />
-          ))}
-        </tbody>
-      </table>
+    <div className='conteinerTL'>
+      <div className='container-fluid d-flex flex-column align-items-center bg-secondary'>
+        <TaskForm onCreateTask={createNewTask} />
+        <h1>Mis tareas</h1>
+        <div className="tableTL">
+          {tasks.length === 0 ? (
+            <p>No hay tareas disponibles</p>
+          ) : (
+            <table className='table table-dark table-striped-row tableTL'>
+              <thead>
+                <tr>
+                  <th className='col-2 tht'>Tareas</th>
+                  <th className='col-1 tht'>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tasks.map((task) => (
+                  <TaskItem
+                    key={task.name}
+                    task={task}
+                    onDelete={deleteTask}
+                    onToggleComplete={toggleComplete}
+                  />
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
